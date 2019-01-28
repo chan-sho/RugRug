e12dc353ffa159acb0924c91f55f89e8296ca0fe
Sho <shoyukimiukanae@gmail.com>
2019-01-28 21:31:07 +0900

    1/28 4th

--- RugRug/New7x7.swift
+++ RugRug/New7x7.swift
@@ -9,7 +9,59 @@
 import UIKit
 
 class New7x7: UIViewController {
-
+    
+    
+    @IBOutlet weak var T0: UITextField!
+    @IBOutlet weak var T1: UITextField!
+    @IBOutlet weak var T2: UITextField!
+    @IBOutlet weak var T3: UITextField!
+    @IBOutlet weak var T4: UITextField!
+    @IBOutlet weak var T5: UITextField!
+    @IBOutlet weak var T6: UITextField!
+    @IBOutlet weak var T7: UITextField!
+    @IBOutlet weak var T8: UITextField!
+    @IBOutlet weak var T9: UITextField!
+    @IBOutlet weak var T10: UITextField!
+    @IBOutlet weak var T11: UITextField!
+    @IBOutlet weak var T12: UITextField!
+    @IBOutlet weak var T13: UITextField!
+    @IBOutlet weak var T14: UITextField!
+    @IBOutlet weak var T15: UITextField!
+    @IBOutlet weak var T16: UITextField!
+    @IBOutlet weak var T17: UITextField!
+    @IBOutlet weak var T18: UITextField!
+    @IBOutlet weak var T19: UITextField!
+    @IBOutlet weak var T20: UITextField!
+    @IBOutlet weak var T21: UITextField!
+    @IBOutlet weak var T22: UITextField!
+    @IBOutlet weak var T23: UITextField!
+    @IBOutlet weak var T24: UITextField!
+    @IBOutlet weak var T25: UITextField!
+    @IBOutlet weak var T26: UITextField!
+    @IBOutlet weak var T27: UITextField!
+    @IBOutlet weak var T28: UITextField!
+    @IBOutlet weak var T29: UITextField!
+    @IBOutlet weak var T30: UITextField!
+    @IBOutlet weak var T31: UITextField!
+    @IBOutlet weak var T32: UITextField!
+    @IBOutlet weak var T33: UITextField!
+    @IBOutlet weak var T34: UITextField!
+    @IBOutlet weak var T35: UITextField!
+    @IBOutlet weak var T36: UITextField!
+    @IBOutlet weak var T37: UITextField!
+    @IBOutlet weak var T38: UITextField!
+    @IBOutlet weak var T39: UITextField!
+    @IBOutlet weak var T40: UITextField!
+    @IBOutlet weak var T41: UITextField!
+    @IBOutlet weak var T42: UITextField!
+    @IBOutlet weak var T43: UITextField!
+    @IBOutlet weak var T44: UITextField!
+    @IBOutlet weak var T45: UITextField!
+    @IBOutlet weak var T46: UITextField!
+    @IBOutlet weak var T47: UITextField!
+    @IBOutlet weak var T48: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ RugRug/Tips.swift
@@ -0,0 +1,30 @@
+//
+//  Tips.swift
+//  RugRug
+//
+//  Created by 高野翔 on 2019/01/28.
+//  Copyright © 2019 高野翔. All rights reserved.
+//
+
+import UIKit
+
+class Tips: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

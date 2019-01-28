f98a49047914313aa75db472d63a4e53f534f137
Sho <shoyukimiukanae@gmail.com>
2019-01-28 22:20:40 +0900

    1/28 5th

--- RugRug/New7x7.swift
+++ RugRug/New7x7.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class New7x7: UIViewController {
+class New7x7: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var T0: UITextField!
@@ -61,22 +61,177 @@ class New7x7: UIViewController {
     @IBOutlet weak var T47: UITextField!
     @IBOutlet weak var T48: UITextField!
     
+    @IBOutlet weak var newPostButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        T0.delegate = self
+        T1.delegate = self
+        T2.delegate = self
+        T3.delegate = self
+        T4.delegate = self
+        T5.delegate = self
+        T6.delegate = self
+        T7.delegate = self
+        T8.delegate = self
+        T9.delegate = self
+        T10.delegate = self
+        T11.delegate = self
+        T12.delegate = self
+        T13.delegate = self
+        T14.delegate = self
+        T15.delegate = self
+        T16.delegate = self
+        T17.delegate = self
+        T18.delegate = self
+        T19.delegate = self
+        T20.delegate = self
+        T21.delegate = self
+        T22.delegate = self
+        T23.delegate = self
+        T24.delegate = self
+        T25.delegate = self
+        T26.delegate = self
+        T27.delegate = self
+        T28.delegate = self
+        T29.delegate = self
+        T30.delegate = self
+        T31.delegate = self
+        T32.delegate = self
+        T33.delegate = self
+        T34.delegate = self
+        T35.delegate = self
+        T36.delegate = self
+        T37.delegate = self
+        T38.delegate = self
+        T39.delegate = self
+        T40.delegate = self
+        T41.delegate = self
+        T42.delegate = self
+        T43.delegate = self
+        T44.delegate = self
+        T45.delegate = self
+        T46.delegate = self
+        T47.delegate = self
+        T48.delegate = self
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Returnボタンを押した際にキーボードを消す
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        T0.resignFirstResponder()
+        T1.resignFirstResponder()
+        T2.resignFirstResponder()
+        T3.resignFirstResponder()
+        T4.resignFirstResponder()
+        T5.resignFirstResponder()
+        T6.resignFirstResponder()
+        T7.resignFirstResponder()
+        T8.resignFirstResponder()
+        T9.resignFirstResponder()
+        T10.resignFirstResponder()
+        T11.resignFirstResponder()
+        T12.resignFirstResponder()
+        T13.resignFirstResponder()
+        T14.resignFirstResponder()
+        T15.resignFirstResponder()
+        T16.resignFirstResponder()
+        T17.resignFirstResponder()
+        T18.resignFirstResponder()
+        T19.resignFirstResponder()
+        T20.resignFirstResponder()
+        T21.resignFirstResponder()
+        T22.resignFirstResponder()
+        T23.resignFirstResponder()
+        T24.resignFirstResponder()
+        T25.resignFirstResponder()
+        T26.resignFirstResponder()
+        T27.resignFirstResponder()
+        T28.resignFirstResponder()
+        T29.resignFirstResponder()
+        T30.resignFirstResponder()
+        T31.resignFirstResponder()
+        T32.resignFirstResponder()
+        T33.resignFirstResponder()
+        T34.resignFirstResponder()
+        T35.resignFirstResponder()
+        T36.resignFirstResponder()
+        T37.resignFirstResponder()
+        T38.resignFirstResponder()
+        T39.resignFirstResponder()
+        T40.resignFirstResponder()
+        T41.resignFirstResponder()
+        T42.resignFirstResponder()
+        T43.resignFirstResponder()
+        T44.resignFirstResponder()
+        T45.resignFirstResponder()
+        T46.resignFirstResponder()
+        T47.resignFirstResponder()
+        T48.resignFirstResponder()
+        
+        return true
     }
-    */
+    
+    
+    // テキスト以外の場所をタッチした際にキーボードを消す
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        T0.resignFirstResponder()
+        T1.resignFirstResponder()
+        T2.resignFirstResponder()
+        T3.resignFirstResponder()
+        T4.resignFirstResponder()
+        T5.resignFirstResponder()
+        T6.resignFirstResponder()
+        T7.resignFirstResponder()
+        T8.resignFirstResponder()
+        T9.resignFirstResponder()
+        T10.resignFirstResponder()
+        T11.resignFirstResponder()
+        T12.resignFirstResponder()
+        T13.resignFirstResponder()
+        T14.resignFirstResponder()
+        T15.resignFirstResponder()
+        T16.resignFirstResponder()
+        T17.resignFirstResponder()
+        T18.resignFirstResponder()
+        T19.resignFirstResponder()
+        T20.resignFirstResponder()
+        T21.resignFirstResponder()
+        T22.resignFirstResponder()
+        T23.resignFirstResponder()
+        T24.resignFirstResponder()
+        T25.resignFirstResponder()
+        T26.resignFirstResponder()
+        T27.resignFirstResponder()
+        T28.resignFirstResponder()
+        T29.resignFirstResponder()
+        T30.resignFirstResponder()
+        T31.resignFirstResponder()
+        T32.resignFirstResponder()
+        T33.resignFirstResponder()
+        T34.resignFirstResponder()
+        T35.resignFirstResponder()
+        T36.resignFirstResponder()
+        T37.resignFirstResponder()
+        T38.resignFirstResponder()
+        T39.resignFirstResponder()
+        T40.resignFirstResponder()
+        T41.resignFirstResponder()
+        T42.resignFirstResponder()
+        T43.resignFirstResponder()
+        T44.resignFirstResponder()
+        T45.resignFirstResponder()
+        T46.resignFirstResponder()
+        T47.resignFirstResponder()
+        T48.resignFirstResponder()
+    }
 
+    
+    @IBAction func newPostButton(_ sender: Any) {
+        
+    }
+    
 }
